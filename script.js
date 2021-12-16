c3c949ba6049c8973e3498b0b001533fd3f0a667
charchit nirayanwal <43198317+charchit011@users.noreply.github.com>
2021-12-16 15:58:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function searchmovie(){
+    var moviename=document.getElementById('moviename').value;
+    // document.getElementById('tata').innerHTML=moviename;
+
+fetch("http://www.omdbapi.com/?apikey=8f595a29&s="+moviename)
+ .then((response)=>{
+     
+     var data=response.json();
+     console.log(data);
+     return data;
+     })
+     .then((data)=>{
+        var names;
+         var p=data.Search;
+        //  var pt=p.json();
+        var count = Object.keys(p).length;
+         console.log(count);
+         for (let i = 0; i < count; i++) {
+         names+=(p[i].Title+'\n');
+         console.log(p[i].Title);
+        }  
+        document.getElementById('tata').innerHTML=names;
+     })
+   
+  .catch()  
+
+    }
+
+    function changetitle(){
+        var movietitle=document.getElementById('moviename').value;
+        document.getElementById('title').innerHTML=movietitle;
+
+    }
